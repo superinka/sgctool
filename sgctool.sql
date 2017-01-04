5b9d4451c0ea9a1a066f9f90a2db309f8d019ce2
superinka <thesuperinka@gmail.com>
2017-01-04 18:01:01 +0700

    sql

--- sgctool.sql
+++ sgctool.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2017 at 04:56 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- Generation Time: Jan 04, 2017 at 11:59 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -48,7 +48,11 @@ CREATE TABLE `tb_daily_report` (
 --
 
 INSERT INTO `tb_daily_report` (`id`, `create_by`, `create_time`, `status`, `task_id`, `update_time`, `description`, `code`, `time_spend`, `progress`, `review_by`, `review_status`, `create_date`, `note`) VALUES
-(1, 47, '2017-01-03 00:00:00.000000', 1, 1, '2017-01-03 00:00:00.000000', 'report hôm nay', '1', 8, 1, 1, 0, '2017-01-03', NULL);
+(1, 47, '2017-01-03 00:00:00.000000', 1, 3, '2017-01-03 00:00:00.000000', 'report hôm nay', '1', 8, 0, 46, 1, '2017-01-03', NULL),
+(2, 47, '2017-01-03 00:00:00.000000', 1, 3, '2017-01-03 00:00:00.000000', 'report hôm nay', '1', 8, 100, 46, 0, '2017-01-03', NULL),
+(3, 47, '2017-01-04 00:00:00.000000', 1, 3, '2017-01-03 00:00:00.000000', 'report hôm nay', '1', 8, 100, 46, 1, '2017-01-04', NULL),
+(4, 47, '2017-01-04 00:00:00.000000', 1, 3, '2017-01-04 00:00:00.000000', 'report hôm nay', '1', 8, 0, 46, 0, '2017-01-04', NULL),
+(5, 47, '2017-01-04 00:00:00.000000', 1, 3, '2017-01-04 00:00:00.000000', 'report hôm nay', '1', 8, 0, 46, 0, '2017-01-04', NULL);
 
 -- --------------------------------------------------------
 
@@ -138,17 +142,21 @@ CREATE TABLE `tb_mission` (
   `start_date` date DEFAULT NULL,
   `end_date` date DEFAULT NULL,
   `code` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `update_by` int(8) DEFAULT NULL
+  `update_by` int(8) DEFAULT NULL,
+  `department_id` int(8) DEFAULT NULL,
+  `level` int(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `tb_mission`
 --
 
-INSERT INTO `tb_mission` (`id`, `name`, `description`, `create_by`, `create_date`, `update_time`, `progress`, `project_id`, `status`, `start_date`, `end_date`, `code`, `update_by`) VALUES
-(12, 'Vẽ con gà', 'ahihi', 46, '2016-12-30', '2017-01-02 17:56:11.000000', 70, 20, 1, '2016-12-30', '2016-12-30', '20596f932feb74cd842311e6b26dd5d09f', 1),
-(13, 'Vẽ con chó', 'Dự án vườn treo', 46, '2016-12-30', '2017-01-02 20:41:40.000000', 50, 20, 1, '2016-12-30', '2016-12-30', '20bd2bc19194002ef15f767fa9f989ff9c', 1),
-(14, 'Vẽ con trâu', '', 1, '2017-01-02', '2017-01-02 20:45:43.000000', 0, 20, 1, '2017-01-02', '2017-01-02', '201350fa0e60b5f469f67d6b47f38aa42ab1eb', 1);
+INSERT INTO `tb_mission` (`id`, `name`, `description`, `create_by`, `create_date`, `update_time`, `progress`, `project_id`, `status`, `start_date`, `end_date`, `code`, `update_by`, `department_id`, `level`) VALUES
+(12, 'Vẽ con gà', 'ahihi', 46, '2016-12-30', '2017-01-02 17:56:11.000000', 70, 20, 1, '2016-12-30', '2016-12-30', '20596f932feb74cd842311e6b26dd5d09f', 1, 8, 4),
+(13, 'Vẽ con chó', 'Dự án vườn treo', 46, '2016-12-30', '2017-01-02 20:41:40.000000', 50, 20, 1, '2016-12-30', '2016-12-30', '20bd2bc19194002ef15f767fa9f989ff9c', 1, 9, 4),
+(14, 'Vẽ con trâu', '', 1, '2017-01-02', '2017-01-02 20:45:43.000000', 0, 20, 1, '2017-01-02', '2017-01-02', '201350fa0e60b5f469f67d6b47f38aa42ab1eb', 1, 9, 4),
+(15, 'Giám sát', 'giám sát', 1, '2017-01-04', '2017-01-04 12:50:01.000000', 100, 21, 1, '2017-01-04', '2017-01-04', '2146805a309091ab1c9c7184d93f41216255c6', 1, 10, 3),
+(16, 'Giám sát phòng', 'giám sát', 1, '2017-01-04', '2017-01-04 14:45:10.000000', 0, 20, 1, '2017-01-04', '2017-01-04', '20492831f30bad41b5ef19fa90b10bbf264786', 1, 9, 3);
 
 -- --------------------------------------------------------
 
@@ -170,7 +178,9 @@ CREATE TABLE `tb_mission_user` (
 INSERT INTO `tb_mission_user` (`id`, `mission_id`, `user_id`, `update_time`) VALUES
 (2, 12, 51, '2017-01-02 15:00:05.000000'),
 (3, 13, 47, '2017-01-02 20:41:40.000000'),
-(4, 14, 47, '2017-01-02 20:45:43.000000');
+(4, 14, 47, '2017-01-02 20:45:43.000000'),
+(5, 15, 52, '2017-01-04 12:50:01.000000'),
+(6, 16, 52, '2017-01-04 14:45:10.000000');
 
 -- --------------------------------------------------------
 
@@ -199,8 +209,11 @@ CREATE TABLE `tb_project` (
 --
 
 INSERT INTO `tb_project` (`id`, `project_name`, `description`, `create_date`, `create_by`, `start_date`, `end_date`, `status`, `department_id`, `progress`, `update_by`, `update_time`, `short_name`) VALUES
-(20, 'Babylon', 'Dự án vườn treo', '2016-12-30', 1, '2016-12-30', '2016-12-30', '1', NULL, 10, 1, '2017-01-03 11:00:44.000000', 'B'),
-(21, 'Babylon 2', 'version 2', '2017-01-03', 1, '2017-01-03', '2017-01-03', '1', NULL, 10, 1, '2017-01-03 21:07:02.000000', 'BB2');
+(21, 'Babylon 2', 'version 2', '2017-01-03', 1, '2017-01-03', '2017-01-03', '1', NULL, 10, 1, '2017-01-04 16:41:26.000000', 'BB2'),
+(25, 'Babylon', 'ahihi', '2017-01-04', 1, '2017-01-04', '2017-01-04', '1', NULL, 100, 1, '2017-01-04 17:36:51.000000', '10'),
+(26, 'BBC', 'Dự án vườn treo', '2017-01-04', 1, '2017-01-04', '2017-01-04', '1', NULL, 100, 1, '2017-01-04 17:38:18.000000', 'B'),
+(27, 'Babylon', 'ahihi 2', '2017-01-04', 1, '2017-01-04', '2017-01-04', '1', NULL, 100, 1, '2017-01-04 17:59:00.000000', 'BBL2'),
+(28, 'Babylon', 'ahihi', '2017-01-04', 1, '2017-01-04', '2017-01-04', '1', NULL, 10, 1, '2017-01-04 17:58:25.000000', '1');
 
 -- --------------------------------------------------------
 
@@ -221,12 +234,24 @@ CREATE TABLE `tb_project_user` (
 --
 
 INSERT INTO `tb_project_user` (`id`, `des`, `project_id`, `user_id`, `update_time`) VALUES
-(101, NULL, 20, 48, '2016-12-30 09:27:30.000000'),
-(102, NULL, 20, 49, '2016-12-30 09:27:30.000000'),
-(103, NULL, 20, 51, '2016-12-30 09:27:30.000000'),
-(104, NULL, 20, 47, '2017-01-02 20:40:38.000000'),
-(105, NULL, 21, 47, '2017-01-03 12:37:15.000000'),
-(106, NULL, 21, 50, '2017-01-03 21:07:02.000000');
+(111, NULL, 21, 47, '2017-01-04 16:36:33.000000'),
+(113, NULL, 20, 50, '2017-01-04 17:00:10.000000'),
+(114, NULL, 23, 48, '2017-01-04 17:27:18.000000'),
+(115, NULL, 23, 47, '2017-01-04 17:27:19.000000'),
+(118, NULL, 23, 50, '2017-01-04 17:27:19.000000'),
+(119, NULL, 24, 48, '2017-01-04 17:35:53.000000'),
+(120, NULL, 25, 48, '2017-01-04 17:36:51.000000'),
+(121, NULL, 25, 47, '2017-01-04 17:36:51.000000'),
+(124, NULL, 25, 50, '2017-01-04 17:36:51.000000'),
+(125, NULL, 26, 48, '2017-01-04 17:38:19.000000'),
+(128, NULL, 26, 50, '2017-01-04 17:38:19.000000'),
+(129, NULL, 27, 48, '2017-01-04 17:39:22.000000'),
+(130, NULL, 27, 47, '2017-01-04 17:39:22.000000'),
+(133, NULL, 27, 50, '2017-01-04 17:39:49.000000'),
+(135, NULL, 28, 48, '2017-01-04 17:57:11.000000'),
+(136, NULL, 28, 47, '2017-01-04 17:57:11.000000'),
+(139, NULL, 28, 50, '2017-01-04 17:57:11.000000'),
+(140, NULL, 28, 51, '2017-01-04 17:58:25.000000');
 
 -- --------------------------------------------------------
 
@@ -247,11 +272,22 @@ CREATE TABLE `tb_proportion_department` (
 --
 
 INSERT INTO `tb_proportion_department` (`id`, `department_id`, `project_id`, `proportion`, `update_time`) VALUES
-(36, 12, 20, 10, '2016-12-30 09:27:30.000000'),
-(37, 8, 20, 40, '2016-12-30 09:27:30.000000'),
-(38, 9, 20, 70, '2017-01-02 20:40:38.000000'),
-(39, 9, 21, 70, '2017-01-03 14:17:22.000000'),
-(40, 10, 21, 30, '2017-01-03 21:07:02.000000');
+(39, 9, 21, 100, '2017-01-03 14:17:22.000000'),
+(44, 12, 23, 0, '2017-01-04 17:27:18.000000'),
+(45, 8, 23, 0, '2017-01-04 17:27:19.000000'),
+(46, 8, 23, 0, '2017-01-04 17:27:19.000000'),
+(47, 10, 23, 0, '2017-01-04 17:27:19.000000'),
+(48, 12, NULL, 0, '2017-01-04 17:36:51.000000'),
+(49, 9, NULL, 0, '2017-01-04 17:36:51.000000'),
+(50, 8, NULL, 0, '2017-01-04 17:36:51.000000'),
+(51, 10, NULL, 0, '2017-01-04 17:36:51.000000'),
+(52, 12, 27, 0, '2017-01-04 17:39:22.000000'),
+(53, 9, 27, 0, '2017-01-04 17:39:22.000000'),
+(54, 8, 27, 0, '2017-01-04 17:39:22.000000'),
+(56, 12, 28, 0, '2017-01-04 17:57:11.000000'),
+(57, 9, 28, 0, '2017-01-04 17:57:11.000000'),
+(58, 8, 28, 0, '2017-01-04 17:57:11.000000'),
+(59, 10, 28, 0, '2017-01-04 17:57:11.000000');
 
 -- --------------------------------------------------------
 
@@ -319,7 +355,8 @@ INSERT INTO `tb_task` (`id`, `name`, `description`, `create_by`, `create_date`, 
 (9, 'Vẽ lại con gà', 'Vẽ móng con gà', 51, '2017-01-02', '2017-01-02', '2017-01-02', 100, 12, '2012e5c3e53011bb768c24eac4602096a063', 20),
 (10, 'Vẽ bụng con gà', 'vẽ bụng gà', 51, '2017-01-02', '2017-01-02', '2017-01-02', 100, 12, '20128fe13fef504c55842aaffb31088c4f47', 20),
 (11, 'Vẽ đầu con chó', 'Vẽ đầu chó', 47, '2017-01-02', '2017-01-02', '2017-01-02', 100, 13, '20135dd40edef059a5d93f4ad7c209267fda', 20),
-(12, 'Vẽ chân chó', 'Vẽ chân chó', 47, '2017-01-02', '2017-01-02', '2017-01-02', 0, 13, '2013d909a02739aca3a41965e9a5b828fb6a', 20);
+(12, 'Vẽ chân chó', 'Vẽ chân chó', 47, '2017-01-02', '2017-01-02', '2017-01-02', 0, 13, '2013d909a02739aca3a41965e9a5b828fb6a', 20),
+(13, 'giám sát con gà', 'giám sát thôi', 52, '2017-01-04', '2017-01-04', '2017-01-04', 100, 15, '211548c725aed179582f72f5d7b487dcfc36', 21);
 
 -- --------------------------------------------------------
 
@@ -432,7 +469,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_daily_report`
 --
 ALTER TABLE `tb_daily_report`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tb_department`
 --
@@ -447,27 +484,27 @@ ALTER TABLE `tb_employee`
 -- AUTO_INCREMENT for table `tb_mission`
 --
 ALTER TABLE `tb_mission`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `tb_mission_user`
 --
 ALTER TABLE `tb_mission_user`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tb_project`
 --
 ALTER TABLE `tb_project`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT for table `tb_project_user`
 --
 ALTER TABLE `tb_project_user`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 --
 -- AUTO_INCREMENT for table `tb_proportion_department`
 --
 ALTER TABLE `tb_proportion_department`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 --
 -- AUTO_INCREMENT for table `tb_role`
 --
@@ -477,7 +514,7 @@ ALTER TABLE `tb_role`
 -- AUTO_INCREMENT for table `tb_task`
 --
 ALTER TABLE `tb_task`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
